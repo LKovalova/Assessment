3cb19041f0ea4328f63385b3b2393816849623c9
Liubov Kovalova <liuba532013@gmail.com>
2024-09-07 08:27:01 -0300

    Initial commit

---
+++ smartHome/src/test/java/com/example/smarthome/SmartHomeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.smarthome;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SmartHomeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
